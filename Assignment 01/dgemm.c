3aa49e651396e1bd4f8f3ec0d0d8a1865ff45e9a
Daniel <57764947+tigerpa616@users.noreply.github.com>
2020-03-05 14:37:23 -0500

    Add files via upload

---
+++ Assignment 01/dgemm.c
@@ -0,0 +1,59 @@
+//
+// Created by Daniel on 1/26/2020.
+//
+
+#include "dgemm.h"
+
+/* initialize matricies */
+void initialize_matricies (int n, double* a, double* b, double* c)
+{
+    for (int i=0; i< ROWLEN; i++) {
+        for (int j = 0; j < ROWLEN; j++) {
+            *(a + i + j * ROWLEN) = (double) rand() / (double) rand();
+            *(b + i + j * ROWLEN) = (double) rand() / (double) rand();
+            *(c + i + j * ROWLEN) = 0;
+        }
+    }
+}
+
+void dgemm (int n, const double *A, const double *B, double *C)
+{
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+        {
+			double cij = C[j+i*n]; /* cij = C[j][i] */
+            for( int k = 0; k < n; k++ )
+                cij += A[k+i*n] * B[j+k*n]; /* cij += A[k][i]*B[j][k] */
+           C[j+i*n] = cij;
+        }
+}
+
+void mm(double a[ROWLEN][ROWLEN], double b[ROWLEN][ROWLEN], double c[ROWLEN][ROWLEN])
+{
+
+    int i,j,k;
+    for (i = 0; i != ROWLEN; i++) {
+        for (j = 0; j != ROWLEN; j++) {
+            for (k = 0; k != ROWLEN; k++) {
+                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+void dgemm_intrins()
+{
+
+}
+
+void showArrary(int n, double* a, double* b, double* c)
+{
+    for (int i = 0; i <  ROWLEN; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("c[%d][%d] = %Lf\n", i,j, c[i+j*ROWLEN] );
+        }
+    }
+}
+
---
+++ Assignment 01/matrix_multiply_2.c
@@ -0,0 +1,38 @@
+#include "dgemm.h"
+
+int main(int argc, char *argv[])
+{
+    // seed random generator
+    srand(2020);
+
+
+
+//    double a[ROWLEN][ROWLEN];
+//    double b[ROWLEN][ROWLEN];
+//    double c[ROWLEN][ROWLEN];
+
+
+a = malloc(ROWLEN * ROWLEN * sizeof(double));
+b = malloc(ROWLEN * ROWLEN * sizeof(double));
+c = malloc(ROWLEN * ROWLEN * sizeof(double));
+
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+
+    initialize_matricies (ROWLEN, a, b, c);
+
+    //mm(a, b, c);
+    dgemm(ROWLEN, a, b, c);
+    dgemm_intrins();
+
+    end = clock();
+    cpu_time_used = ((double)(end - start))/CLOCKS_PER_SEC;
+    printf("the elapsed CPU time is %Lf\n", cpu_time_used);
+    printf("Clocks per sec: %d\n", CLOCKS_PER_SEC);
+
+    showArrary(ROWLEN, a, b, c);
+    return 0;
+}
+
+
